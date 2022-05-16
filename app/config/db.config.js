ce7500e02bd3ac3f7a2b166131f3bd6c59cfd093
JD AS IA <assia>
2022-05-17 00:08:21 +0100

    modifier name db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: "mongodb+srv://Assia_Jdey:24251586@cluster0.f4mcp.mongodb.net/?retryWrites=true&w=majority"
+    url: "mongodb+srv://Assia_Jdey:24251586@cluster0.f4mcp.mongodb.net/projetData?retryWrites=true&w=majority"
   };
   
